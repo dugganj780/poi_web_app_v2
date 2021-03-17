ae75b6e8428d8a4f48587f0df43c74d1a54feec2
dugganj780 <dugganj780@gmail.com>
2021-03-17 13:09:57 +0000

    Deploying

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ require('./app/models/db');
 env.config();
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
 });
 
 const credentials = {
